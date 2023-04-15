a655356b96609ad7b3581cbcab29b75e70d882f1
Tomek Zubrzycki <zpt77@wp.pl>
2023-04-15 15:26:14 +0200

    timer

--- src/main/java/com/example/springserver/SpringServerApplication.java
+++ src/main/java/com/example/springserver/SpringServerApplication.java
@@ -1,22 +1,45 @@
 package com.example.springserver;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @SpringBootApplication
 @RestController
 public class SpringServerApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringServerApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
+	private LocalDateTime startupTime;
+
+	@PostConstruct
+	public void init() {
+		startupTime = LocalDateTime.now();
 	}
+
+	@GetMapping("/")
+	public ResponseEntity<String> main() {
+		String html = "<html><body><h1>Spring Server is running!</h1><p>Started at: " + getStartupTime() + "</p></body></html>";
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.TEXT_HTML);
+		return new ResponseEntity<>(html, headers, HttpStatus.OK);
+	}
+
+	private String getStartupTime() {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+		return startupTime.format(formatter);
+	}
+
 
 	@PostMapping("/login")
 	public ResponseEntity<String> login(@RequestBody LoginData loginData) {
